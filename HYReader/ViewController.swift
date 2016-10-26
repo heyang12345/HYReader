9519611b8eeeb459ed660986d0dc1afe900a58de
heyang <2279598898@qq.com>
2016-10-26 22:17:34 +0800

    Initial Commit

---
+++ HYReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HYReader
+//
+//  Created by 千锋 on 16/10/26.
+//  Copyright © 2016年 heyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
